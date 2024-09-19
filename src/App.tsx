87730ef359bfed7e2bb36ec3ab01633c9487e28d
bvilla1 <bvilla1@cuc.edu.co>
2024-09-19 11:35:11 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import miLogo from "./assets/img/calavera.png"
+
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <img src={miLogo} alt="Logo de react" />
+      <h1>MEXICO</h1>
+      <p> este es mi primer proyecto en react</p>
+    </div>
+    </>
+  )
+}
+
+export default App
